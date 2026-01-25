3c894030637a0a55cf6baad2241019a3a3cf8afb
jinxIsOp <154748750+jinxIsOp@users.noreply.github.com>
2026-01-25 13:36:40 +0100

    hm hm hm

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AgilDashboard/', // Must match your repo name EXACTLY
+  base: './',
   server: {
     port: 3000,
     open: true
   },
   build: {
     outDir: 'dist',
-    chunkSizeWarningLimit: 1000,
+    emptyOutDir: true,
   }
 })
